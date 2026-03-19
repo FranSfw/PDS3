af616b3b70a546258cd569767b20d0bec2bbaa2b
Francisco Perez <117410205+FranSfw@users.noreply.github.com>
2026-03-19 13:35:51 -0700

    comienzo de chats y seguimiento de edicion

--- app/src/main/java/com/example/appcomprayventa/EditarPerfil.kt
+++ app/src/main/java/com/example/appcomprayventa/EditarPerfil.kt
@@ -1,13 +1,24 @@
 package com.example.appcomprayventa
 
 import android.app.ProgressDialog
+import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.example.appcomprayventa.databinding.ActivityEditarPerfilBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class EditarPerfil : AppCompatActivity() {
 
@@ -19,5 +30,138 @@ class EditarPerfil : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditarPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Por favor espere")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        cargarInfo()
+
+        binding.FABCambiarImg.setOnClickListener {
+            selec_imagen_de()
+        }
+
     }
+
+    private fun cargarInfo() {
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val nombres = "${snapshot.child("nombres").value}"
+                    val imagen = "${snapshot.child("urlImagenPerfil").value}"
+                    val f_nac = "${snapshot.child("fecha_nac").value}"
+                    val telefono = "${snapshot.child("telefono").value}"
+                    val codTelefono = "${snapshot.child("codigoTelefono").value}"
+
+                    //Establecer los valores
+                    binding.EtNombres.setText(nombres)
+                    binding.EtFNac.setText(f_nac)
+                    binding.EtTelefono.setText(telefono)
+
+                    try {
+                        Glide.with(applicationContext)
+                            .load(imagen)
+                            .placeholder(R.drawable.img_perfil)
+                            .into(binding.imgPerfil)
+                    }catch(e: Exception){
+                        Toast.makeText(this@EditarPerfil,
+                            "${e.message}",
+                            Toast.LENGTH_SHORT).show()
+                    }
+
+                    try {
+                        val codigo = codTelefono.replace("+", "").toInt()
+                        binding.selectorCod.setCountryForPhoneCode(codigo)
+                    }catch(e: Exception){
+                        Toast.makeText(this@EditarPerfil,
+                            "${e.message}",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+    }
+
+    private fun selec_imagen_de(){
+        val popupMenu = PopupMenu(this, binding.FABCambiarImg)
+
+        popupMenu.menu.add(Menu.NONE, 1, 1, "Camara")
+        popupMenu.menu.add(Menu.NONE, 2, 2, "Galeria")
+
+        popupMenu.show()
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            val itemId = item.itemId
+            if (itemId == 1){
+                //camara
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    concederPermisoCamara.launch(arrayOf(android.Manifest.permission.CAMERA))
+                }else {
+                    concederPermisoCamara.launch(arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ))
+                }
+
+            }else if (itemId == 2){
+                //galeria
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    imagenGaleria()
+                }else {
+                    concederPermisoCamara.launch(arrayOf(
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ))
+                }
+            }
+            return@setOnMenuItemClickListener true
+
+        }
+
+    }
+
+    private val concederPermisoCamara =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()) { resultado ->
+            var concedidoTodos = true
+            for (seConcede in resultado.values) {
+                concedidoTodos = concedidoTodos && seConcede
+            }
+
+            if (concedidoTodos) {
+                imagenCamara()
+            }else{
+                Toast.makeText(this,
+                    "No se concedieron permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private fun imagenCamara() {
+        TODO("Not yet implemented")
+    }
+
+    private val concederPermisosAlmacenamiento =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){ esConcedido ->
+            if (esConcedido) {
+                imagenGaleria()
+            } else {
+                Toast.makeText(this,
+                    "No se concedieron permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
+    private fun imagenGaleria() {
+        TODO("Not yet implemented")
+    }
+
 }
